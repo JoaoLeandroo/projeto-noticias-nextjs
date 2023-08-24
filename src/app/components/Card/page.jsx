005695b45edb8dcbffe5b25ad15eae03a12f358f
joaoleandro <new.joaoleandro@gmail.com>
2023-08-24 18:44:25 -0300

    noticias

--- src/app/components/Card/page.jsx
+++ src/app/components/Card/page.jsx
@@ -5,21 +5,16 @@ import Image from "next/image"
 export default function Card(props) {
       
     return(
-        <section className="w-80 h-56 bg-black rounded-xl relative p-3 transition-all hover:w-[730px] cursor-pointer">
-            <h2 className="font-bold text-white text-center">{props.tittle}</h2>
+        <section className="w-80 h-56 bg-black rounded-xl relative transition-all cursor-pointer">
+            <h2 className="font-bold text-white text-center absolute z-20 w-full bg-purple-700 rounded-t-[12px] p-1">{props.tittle}</h2>
             <Image 
             loader={props.image}
             src="me.png"
             alt={props.texto}
             width={320}
             height={320}
-            className="absolute bottom-0 left-0 rounded-b-[12px]"
+            className="absolute bottom-0 left-0 rounded-b-[12px] bg-cover bg-center"
             />
-            <div className="w-[320px] absolute h-full hover:right-[0] transition-all duration-300 ease-in-out">
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores facere ducimus tenetur libero eius cumque explicabo accusantium.
-                </p>
-            </div>
         </section>
     )
 }
--- src/app/pages/Noticias/page.jsx
+++ src/app/pages/Noticias/page.jsx
@@ -6,11 +6,36 @@ const imageTeste = () => {
     return "https://img.olhardigital.com.br/wp-content/uploads/2022/12/Destaque-gravidade-na-lua.jpg"
 }
 
+const imageCristianoRonaldo = () => {
+    return "https://1xbet.tv/wp-content/uploads/2023/08/tj5pyte4aqhb1.jpg"
+}
+
+const imageFormula1 = () => {
+    return "https://blog.pneustore.com.br/wp-content/uploads/2023/05/max-verstappen-da-red-bull-durante-os-treinos-livres-do-gp-de-miami-de-f1-1683324424614_v2_1920x1279-825x510.jpg"
+}
+
+const imageFilme = () => {
+    return "https://uploads.metropoles.com/wp-content/uploads/2023/07/18152459/Barbie-15_Easy-Resize.com_-600x400.jpg"
+}
+
+const imageFutebol = () => {
+    return "https://www.latercera.com/resizer/tQTdcdWqKbLaD7Hh6TZrESsG6uM=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/C76BBJTM6XXEPGP34HIF2UFYCA.jpg"
+}
+
+const imageWar = () => {
+    return "https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3191661:1644606762/russo.jpeg?f=default&$p$f=b37fdd3"
+}
+
 export default function Noticias() {
 
     return(
-        <main className="p-4">
-            <Card tittle="Astronauta" image={imageTeste} texto="xxxxx"/>
+        <main className="p-5 max-w-[95%] m-auto mt-5 flex gap-3 justify-center flex-wrap">
+            <Card tittle="Homem de volta a Lua?" image={imageTeste} texto="a humanidade está pronta para voltar a lua"/>
+            <Card tittle="SEMPRE ELE!!!" image={imageCristianoRonaldo} texto="Jogador Cristiano Ronaldo"/>
+            <Card tittle="Sucesso de bilheteria" image={imageFilme} texto="filme da Barbie"/>
+            <Card tittle="Verdão Imparável" image={imageFutebol} texto="Palmeira goleia"/>
+            <Card tittle="Formula 1 de volta" image={imageFormula1} texto="Formula 1 volta esse final de semana"/>
+            <Card tittle="Conflito sem Fim" image={imageWar} texto="Guerra entre Russia e Ucrania"/>
         </main>
     )
 }
